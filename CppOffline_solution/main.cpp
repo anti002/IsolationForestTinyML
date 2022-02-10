6bb986fe5acbac1a163a88d0eb83d9dddf6ab38f
anti002 <anttib-5@student.ltu.se>
2022-02-10 09:27:38 +0100

    Added sciForest for ofline solution training

---
+++ CppOffline_solution/main.cpp
@@ -0,0 +1,19 @@
+class antonForest {
+
+public:
+
+    int t_trees;
+    int sample_size;
+
+    antonForest(int t_trees, int sample_size)
+    {
+
+    }
+
+    
+    antonForest fit()
+    {
+        
+    };
+    
+};
