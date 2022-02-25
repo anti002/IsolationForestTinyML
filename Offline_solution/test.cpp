f15e3e53e5587a61c4225de5f2b71d84c36bce96
anti002 <anttib-5@student.ltu.se>
2022-02-25 11:17:54 +0100

    Fixed error in scoring, now test.cpp gets identical socres as sKiForest

--- Offline_solution/test.cpp
+++ Offline_solution/test.cpp
@@ -57,99 +57,76 @@ int main(){
 
     parseCSV();
 
-    iTree1.push_back({1, 92, 4, 129, 140.76942206675625});
-    iTree1.push_back({2, 43, 4, 127, 90.76112199563585});
-    iTree1.push_back({3, 18, 6, 54, 0.8489511963225397});
-    iTree1.push_back({4, 5, 3, 12, 13.537867511899258});
+    
+    iTree1.push_back({1, 70, 2, 129, 3.001993747746314});
+    iTree1.push_back({2, 3, 5, 128, 1.0984851564639742});
     iTree1.push_back({0, 0, -2, 1, -2.0});
-    iTree1.push_back({6, 9, 1, 11, 2.476501037700126});
-    iTree1.push_back({7, 8, 7, 2, 0.5455851492290039});
-    iTree1.push_back({0, 0, -2, 1, -2.0});
-    iTree1.push_back({0, 0, -2, 1, -2.0});
-    iTree1.push_back({10, 13, 0, 9, 12.837502419160215});
-    iTree1.push_back({11, 12, 9, 2, 6.573685979083372});
+    iTree1.push_back({4, 23, 7, 127, 0.28393284182323786});
+    iTree1.push_back({5, 22, 12, 30, 996.0485382965061});
+    iTree1.push_back({6, 9, 0, 29, 11.801234189826335});
+    iTree1.push_back({7, 8, 6, 2, 2.462179098513871});
     iTree1.push_back({0, 0, -2, 1, -2.0});
     iTree1.push_back({0, 0, -2, 1, -2.0});
-    iTree1.push_back({14, 17, 6, 7, 0.7135073439171333});
-    iTree1.push_back({15, 16, 0, 6, 13.621330615025457});
-    iTree1.push_back({0, 0, -2, 3, -2.0});
-    iTree1.push_back({0, 0, -2, 3, -2.0});
-    iTree1.push_back({0, 0, -2, 1, -2.0});
-    iTree1.push_back({19, 42, 10, 42, 1.6199856722022368});
-    iTree1.push_back({20, 29, 8, 41, 1.306085388580287});
-    iTree1.push_back({21, 22, 3, 6, 16.642311424316052});
-    iTree1.push_back({0, 0, -2, 1, -2.0});
-    iTree1.push_back({23, 26, 5, 5, 2.125494032511546});
-    iTree1.push_back({24, 25, 5, 2, 1.9350272501571635});
-    iTree1.push_back({0, 0, -2, 1, -2.0});
-    iTree1.push_back({0, 0, -2, 1, -2.0});
-    iTree1.push_back({27, 28, 6, 3, 1.9860975224284452});
-    iTree1.push_back({0, 0, -2, 1, -2.0});
-    iTree1.push_back({0, 0, -2, 2, -2.0});
-    iTree1.push_back({30, 37, 3, 35, 18.847464016102006});
-    iTree1.push_back({31, 34, 1, 9, 1.3631390551044111});
-    iTree1.push_back({32, 33, 8, 4, 1.8730474828800352});
-    iTree1.push_back({0, 0, -2, 2, -2.0});
-    iTree1.push_back({0, 0, -2, 2, -2.0});
-    iTree1.push_back({35, 36, 1, 5, 1.4687860365609782});
-    iTree1.push_back({0, 0, -2, 1, -2.0});
-    iTree1.push_back({0, 0, -2, 4, -2.0});
-    iTree1.push_back({38, 39, 11, 26, 1.890215280977992});
-    iTree1.push_back({0, 0, -2, 1, -2.0});
-    iTree1.push_back({40, 41, 0, 25, 12.575581406219564});
-    iTree1.push_back({0, 0, -2, 20, -2.0});
-    iTree1.push_back({0, 0, -2, 5, -2.0});
-    iTree1.push_back({0, 0, -2, 1, -2.0});
-    iTree1.push_back({44, 53, 2, 73, 1.9412826931393372});
-    iTree1.push_back({45, 48, 10, 5, 1.1360814102807755});
-    iTree1.push_back({46, 47, 4, 2, 97.20512599740417});
-    iTree1.push_back({0, 0, -2, 1, -2.0});
-    iTree1.push_back({0, 0, -2, 1, -2.0});
-    iTree1.push_back({49, 52, 5, 3, 2.1613230639030436});
-    iTree1.push_back({50, 51, 5, 2, 1.7041187211664692});
-    iTree1.push_back({0, 0, -2, 1, -2.0});
-    iTree1.push_back({0, 0, -2, 1, -2.0});
-    iTree1.push_back({0, 0, -2, 1, -2.0});
-    iTree1.push_back({54, 81, 6, 68, 2.985762086712655});
-    iTree1.push_back({55, 66, 4, 62, 96.0752850422055});
-    iTree1.push_back({56, 63, 12, 14, 713.1590439626757});
-    iTree1.push_back({57, 60, 10, 12, 0.6174441616249634});
-    iTree1.push_back({58, 59, 8, 2, 1.2527153101716906});
-    iTree1.push_back({0, 0, -2, 1, -2.0});
-    iTree1.push_back({0, 0, -2, 1, -2.0});
-    iTree1.push_back({61, 62, 6, 10, 0.7309903702598203});
-    iTree1.push_back({0, 0, -2, 5, -2.0});
-    iTree1.push_back({0, 0, -2, 5, -2.0});
-    iTree1.push_back({64, 65, 11, 2, 1.6234693625022187});
-    iTree1.push_back({0, 0, -2, 1, -2.0});
-    iTree1.push_back({0, 0, -2, 1, -2.0});
-    iTree1.push_back({67, 74, 8, 48, 1.2372903239313504});
-    iTree1.push_back({68, 71, 11, 19, 2.0126747956771096});
-    iTree1.push_back({69, 70, 2, 13, 2.2014979868734468});
-    iTree1.push_back({0, 0, -2, 2, -2.0});
-    iTree1.push_back({0, 0, -2, 11, -2.0});
-    iTree1.push_back({72, 73, 3, 6, 21.562002563645134});
-    iTree1.push_back({0, 0, -2, 3, -2.0});
-    iTree1.push_back({0, 0, -2, 3, -2.0});
-    iTree1.push_back({75, 78, 3, 29, 18.9790093885575});
-    iTree1.push_back({76, 77, 2, 10, 2.0492380706255524});
-    iTree1.push_back({0, 0, -2, 1, -2.0});
+    iTree1.push_back({10, 17, 12, 27, 661.2775011599424});
+    iTree1.push_back({11, 14, 0, 21, 13.339808253514153});
+    iTree1.push_back({12, 13, 1, 17, 1.3756862994552208});
+    iTree1.push_back({0, 0, -2, 8, -2.0});
     iTree1.push_back({0, 0, -2, 9, -2.0});
-    iTree1.push_back({79, 80, 6, 19, 2.8185074995494475});
-    iTree1.push_back({0, 0, -2, 18, -2.0});
-    iTree1.push_back({0, 0, -2, 1, -2.0});
-    iTree1.push_back({82, 91, 2, 6, 2.7447860646386926});
-    iTree1.push_back({83, 88, 9, 5, 6.422476482026762});
-    iTree1.push_back({84, 85, 3, 3, 16.65358340452632});
-    iTree1.push_back({0, 0, -2, 1, -2.0});
-    iTree1.push_back({86, 87, 0, 2, 13.311816107057368});
-    iTree1.push_back({0, 0, -2, 1, -2.0});
-    iTree1.push_back({0, 0, -2, 1, -2.0});
-    iTree1.push_back({89, 90, 5, 2, 2.995063489647532});
+    iTree1.push_back({15, 16, 4, 4, 94.37649366556504});
+    iTree1.push_back({0, 0, -2, 2, -2.0});
+    iTree1.push_back({0, 0, -2, 2, -2.0});
+    iTree1.push_back({18, 21, 2, 6, 2.405038238620369});
+    iTree1.push_back({19, 20, 4, 5, 112.58449735100591});
+    iTree1.push_back({0, 0, -2, 3, -2.0});
+    iTree1.push_back({0, 0, -2, 2, -2.0});
     iTree1.push_back({0, 0, -2, 1, -2.0});
     iTree1.push_back({0, 0, -2, 1, -2.0});
+    iTree1.push_back({24, 43, 2, 97, 2.1954620702932384});
+    iTree1.push_back({25, 34, 12, 20, 580.7731478655586});
+    iTree1.push_back({26, 27, 6, 15, 1.1482157041960317});
     iTree1.push_back({0, 0, -2, 1, -2.0});
-    iTree1.push_back({93, 94, 12, 2, 758.5235508352162});
+    iTree1.push_back({28, 31, 4, 14, 96.8548106070863});
+    iTree1.push_back({29, 30, 12, 11, 287.63854161726243});
+    iTree1.push_back({0, 0, -2, 1, -2.0});
+    iTree1.push_back({0, 0, -2, 10, -2.0});
+    iTree1.push_back({32, 33, 6, 3, 1.672542772913862});
+    iTree1.push_back({0, 0, -2, 2, -2.0});
+    iTree1.push_back({0, 0, -2, 1, -2.0});
+    iTree1.push_back({35, 38, 6, 5, 1.7471065887242392});
+    iTree1.push_back({36, 37, 12, 2, 663.7050281947968});
+    iTree1.push_back({0, 0, -2, 1, -2.0});
+    iTree1.push_back({0, 0, -2, 1, -2.0});
+    iTree1.push_back({39, 40, 5, 3, 1.926022271183056});
+    iTree1.push_back({0, 0, -2, 1, -2.0});
+    iTree1.push_back({41, 42, 5, 2, 2.108021920431228});
+    iTree1.push_back({0, 0, -2, 1, -2.0});
+    iTree1.push_back({0, 0, -2, 1, -2.0});
+    iTree1.push_back({44, 59, 1, 77, 3.7524134812585794});
+    iTree1.push_back({45, 52, 9, 59, 9.823295001416081});
+    iTree1.push_back({46, 49, 5, 53, 2.734010337929947});
+    iTree1.push_back({47, 48, 8, 44, 1.286949449167675});
+    iTree1.push_back({0, 0, -2, 21, -2.0});
+    iTree1.push_back({0, 0, -2, 23, -2.0});
+    iTree1.push_back({50, 51, 0, 9, 13.288019892552803});
+    iTree1.push_back({0, 0, -2, 4, -2.0});
+    iTree1.push_back({0, 0, -2, 5, -2.0});
+    iTree1.push_back({53, 56, 9, 6, 10.725589347545405});
+    iTree1.push_back({54, 55, 8, 4, 1.3203436166589797});
+    iTree1.push_back({0, 0, -2, 2, -2.0});
+    iTree1.push_back({0, 0, -2, 2, -2.0});
+    iTree1.push_back({57, 58, 7, 2, 0.5241137351697017});
+    iTree1.push_back({0, 0, -2, 1, -2.0});
+    iTree1.push_back({0, 0, -2, 1, -2.0});
+    iTree1.push_back({60, 67, 2, 18, 2.6440716025218216});
+    iTree1.push_back({61, 64, 4, 16, 97.1213274342573});
+    iTree1.push_back({62, 63, 8, 11, 0.918828054691188});
+    iTree1.push_back({0, 0, -2, 2, -2.0});
+    iTree1.push_back({0, 0, -2, 9, -2.0});
+    iTree1.push_back({65, 66, 7, 5, 0.3868779005231979});
+    iTree1.push_back({0, 0, -2, 2, -2.0});
+    iTree1.push_back({0, 0, -2, 3, -2.0});
+    iTree1.push_back({68, 69, 8, 2, 1.478487676481142});
+    iTree1.push_back({0, 0, -2, 1, -2.0});
     iTree1.push_back({0, 0, -2, 1, -2.0});
     iTree1.push_back({0, 0, -2, 1, -2.0});
     iForest.push_back(iTree1);
@@ -182,10 +159,9 @@ int main(){
                     length += 1;
                 }
             }
-            
             float leaf_size = tree[current_node_id].n_samples;
+            
             float path_length = length + c(leaf_size);
-            std::cout << path_length << std::endl;
             path.push_back(path_length);
         }
 
@@ -204,15 +180,15 @@ int main(){
     float score = 0;
     std::vector<float> average_length = edges;
     for (int i = 0; i < average_length.size(); i ++){
-        score = pow(2, (-1 * average_length[i])/c(71));
-        float scorep =  -1 * score + 0.52;
+        score = pow(2, (-1 * average_length[i])/c(parsedCsv.size()));
+        float scorep =  0.5 - score;
         scores.push_back(scorep);
     }
     edges.clear();
 
     for (size_t i = 0; i < scores.size(); i++)
     {
-        //std::cout << scores[i] << std::endl;
+        std::cout << scores[i] << std::endl;
     }
     
 }
